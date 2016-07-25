f490eee343ef8a34baffc45707d37f4a2bb36fe0
Zi Yan <ziy@nvidia.com>
2016-07-25 14:23:39 -0500

    migrate: Add copy_page_dma to use DMA Engine to copy pages.
    vm.use_all_dma_chans will grab all usable DMA channels
    vm.limit_dma_chans will limit how many DMA channels in use
    
    Signed-off-by: Zi Yan <ziy@nvidia.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -96,6 +96,9 @@
 
 #if defined(CONFIG_SYSCTL)
 
+extern int use_all_dma_chans;
+extern int limit_dma_chans;
+
 /* External variables not in a header file. */
 extern int suid_dumpable;
 #ifdef CONFIG_COREDUMP
@@ -1351,6 +1354,23 @@ static struct ctl_table vm_table[] = {
 		.proc_handler   = &hugetlb_mempolicy_sysctl_handler,
 	},
 #endif
+	 {
+		.procname	= "use_all_dma_chans",
+		.data		= &use_all_dma_chans,
+		.maxlen		= sizeof(use_all_dma_chans),
+		.mode		= 0644,
+		.proc_handler	= sysctl_dma_page_migration,
+		.extra1		= &zero,
+		.extra2		= &one,
+	 },
+	 {
+		.procname	= "limit_dma_chans",
+		.data		= &limit_dma_chans,
+		.maxlen		= sizeof(limit_dma_chans),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+		.extra1		= &zero,
+	 },
 	 {
 		.procname	= "hugetlb_shm_group",
 		.data		= &sysctl_hugetlb_shm_group,
