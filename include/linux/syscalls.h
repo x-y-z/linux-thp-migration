ec0496677366db72b3232460bd8a1521c53eefd1
Zi Yan <ziy@nvidia.com>
2016-07-26 17:29:22 -0500

    mm: migrate: Add exchange_pages syscall to exchange two page lists.
    This can save calling two move_pages().
    
    Signed-off-by: Zi Yan <ziy@nvidia.com>

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -736,6 +736,11 @@ asmlinkage long sys_move_pages(pid_t pid, unsigned long nr_pages,
 				const int __user *nodes,
 				int __user *status,
 				int flags);
+asmlinkage long sys_exchange_pages(pid_t pid, unsigned long nr_pages,
+				const void __user * __user *from_pages,
+				const void __user * __user *to_pages,
+				int __user *status,
+				int flags);
 asmlinkage long sys_mbind(unsigned long start, unsigned long len,
 				unsigned long mode,
 				const unsigned long __user *nmask,
