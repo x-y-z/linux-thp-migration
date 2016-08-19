ff5071b59ee23036e5b02134c936542ce477019a
Zi Yan <ziy@nvidia.com>
2016-08-19 10:10:29 -0500

    mm: migrate: Add mode parameter to support additional page copy routines.
    migrate_page_copy() and copy_huge_page() are affected.
    
    Signed-off-by: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Zi Yan <zi.yan@cs.rutgers.edu>

--- fs/ubifs/file.c
+++ fs/ubifs/file.c
@@ -1482,7 +1482,7 @@ static int ubifs_migrate_page(struct address_space *mapping,
 		SetPagePrivate(newpage);
 	}
 
-	migrate_page_copy(newpage, page);
+	migrate_page_copy(newpage, page, 0);
 	return MIGRATEPAGE_SUCCESS;
 }
 #endif
