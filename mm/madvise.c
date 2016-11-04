11c2b1d29de094740a13cdc2de14469ec5bf149a
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2016-11-04 18:33:59 +0900

    mm: thp: check pmd migration entry in common path
    If one of callers of page migration starts to handle thp, memory management code
    start to see pmd migration entry, so we need to prepare for it before enabling.
    This patch changes various code point which checks the status of given pmds in
    order to prevent race between thp migration and the pmd-related works.
    
    Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    ---
    ChangeLog v1 -> v2:
    - introduce pmd_related() (I know the naming is not good, but can't think up
      no better name. Any suggesntion is welcomed.)

--- mm/madvise.c
+++ mm/madvise.c
@@ -274,7 +274,7 @@ static int madvise_free_pte_range(pmd_t *pmd, unsigned long addr,
 	unsigned long next;
 
 	next = pmd_addr_end(addr, end);
-	if (pmd_trans_huge(*pmd))
+	if (pmd_related(*pmd))
 		if (madvise_free_huge_pmd(tlb, vma, pmd, addr, next))
 			goto next;
 
